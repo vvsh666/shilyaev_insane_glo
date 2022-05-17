3cd73bf26758c6737a595cfa009759c26f93d829
svv <vvsh1978@mail.ru>
2022-05-17 15:35:08 +0500

    feat: change project structure: setup webpack on two output points

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
+    entry: {
+        "admin/js/main": './admin.js',
+        "js/main": './index.js'
+    },
     output: {
-        filename: 'js/main.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
